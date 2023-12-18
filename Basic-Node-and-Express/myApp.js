9f115bc5148fbef1e5ed4ca8f0c33b1d0c415204
catherinepapad <katerinapapado.01@gmail.com>
2023-12-18 23:54:43 +0200

    Add Task 5

--- Basic-Node-and-Express/myApp.js
+++ Basic-Node-and-Express/myApp.js
@@ -16,11 +16,16 @@ console.log("Hello World");
 app.use("/public",express.static(__dirname + "/public"));
 
 
-
-// Task3
+// Task 3
 app.get("/",function(GET, res){
-    absolutePath = __dirname + '/views/index.html'
+    absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
+});
+
+
+// Task 5
+app.get("/json", function(GET, res){
+    res.json({"message": "Hello json"});
 });
 
 
